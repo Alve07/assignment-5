6a7ee7ca77d1e51a8b6a5d4d4e6d0bd7223a20c6
Rid_Haque <ridhaque67@gmail.com>
2024-02-20 16:23:40 +0600

    Calculation done

--- ticket.js/ticket.js
+++ ticket.js/ticket.js
@@ -3,6 +3,7 @@ let n = 0;
 let x = 0;
 let y = 4;
 function ticketCalculation(elementId) {
+
     n = n + 1;
     x = x + 1;
     y = y + 1;
@@ -14,6 +15,13 @@ function ticketCalculation(elementId) {
         totalTicketCost();
         showElementById(x);
         setElementValueById(elementId, y)
+        seatStatusSmall()
+        const elementBtn = document.getElementById(elementId)
+        elementBtn.removeAttribute('onclick')
+        seatSelected();
+
+
+
     }
     else {
         alert("A person can purchase only 4 Tickets!")
--- ticket.js/utility.js
+++ ticket.js/utility.js
@@ -3,6 +3,11 @@ function getInnerTextById(elementId) {
     const elementValue = element.innerText;
     return elementValue
 }
+function getInnerTextValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValue = element.value;
+    return elementValue
+}
 
 function addBackgroundById(element) {
     element.classList.add('bg-[#1DD100]');
@@ -18,12 +23,13 @@ function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
 
-const arr = [];
+
 function availableSeatStatus(elementId) {
-
-    arr.push(elementId);
-    console.log(arr)
     const availableSeatElement = document.getElementById('available-seat')
     const availableSeatText = availableSeatElement.innerText;
     const availableSeatInt = parseInt(availableSeatText);
@@ -38,5 +44,66 @@ function totalTicketCost() {
     const totalCostInt = parseInt(totalCostText);
     const totalCost = totalCostInt + 550;
     setElementValueById(totalCost, 'total-cost')
+    return totalCost;
+
+}
+
+function seatStatusSmall() {
+    const elementText = getInnerTextById('seat-status-small')
+    const elementInt = parseInt(elementText);
+    console.log(elementInt)
+    const element = elementInt + 1;
+    setElementValueById(element, 'seat-status-small')
+
+
+}
+
+function discountCode(couponCode) {
+
+    return code;
+}
+
+let s = 0;
+function seatSelected() {
+    s = s + 1;
+}
+
+function discountedPrice() {
+    const couponCode = getInnerTextValueById('grand-coupon');
+    console.log(couponCode)
+
+
+    if (s === 4) {
+        let code = 0;
+        let x = 0
+        if (couponCode === 'NEW15') {
+            code = 0.15;
+            x = 1
+
+        }
+        else if (couponCode === "Couple20") {
+            code = 0.20;
+            x = 1
+        }
+        else {
+            x = 0
+            alert('Invalid Code');
+        }
+
+        console.log(code);
+        const totalFairText = getInnerTextById('total-cost');
+        const totalFair = parseInt(totalFairText);
+
+        const grandTotal = totalFair - (totalFair * code);
+        console.log(grandTotal);
+        hideElementById('grand-coupon');
+        hideElementById('discount-apply');
+        showElementById('alert-coupon');
+        setElementValueById(grandTotal, 'grand-total');
+    }
+
+    else {
+        alert("Buy at-least 4 tickets to avail coupon")
+    }
 
 }
